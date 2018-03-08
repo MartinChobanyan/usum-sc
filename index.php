3b1da61bcf7329b90051ce2f81408d1d42155a23
Martin Chobanyan <foxmart36@gmail.com>
2018-03-08 22:56:43 +0400

    Starting!
    Barevner mtutyunic

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <link rel="stylesheet" href="css/main.css"/>
+</head>
+<body>
+<!--Verevi zibil-->
+<div class="verev"><div id="logo" style="padding-left: 30%"><img src=""<!--<<<<LOGOIMAGE-->></div></div>
+<div class="middle"><div id="1"></div><div id="2"></div></div>
+<div class="floor"></div>
+</body>
+</html>
